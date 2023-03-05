afe90da599878621269402094adba39028be3a4f
mateushlsilva <mateushls01@gmail.com>
2023-03-05 10:56:10 -0300

    Função com return embutido

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,15 @@ fn nome(n: String){
     println!("Hello, {n}");
 }
 
+fn five() -> i8 { // Função com return embutido
+    5
+}
+
 fn main() {
     println!("Hello, world!");
     teste_funcao();
-    nome("mateus".to_string())
+    nome("mateus".to_string());
+    let x = five();
+
+    println!("{x}");
 }
