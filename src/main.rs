1397d27d4d3f3dff70a357deec19e39d08ebf8e4
mateushlsilva <mateushls01@gmail.com>
2023-03-05 11:01:21 -0300

    função mais_um

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,16 @@ fn five() -> i8 { // Função com return embutido
     5
 }
 
+fn mais_um(i: i32) -> i32 {
+    i + 1
+}
+
 fn main() {
     println!("Hello, world!");
     teste_funcao();
     nome("mateus".to_string());
     let x = five();
+    let i = mais_um(5);
 
-    println!("{x}");
+    println!("{x}, {i}");
 }
